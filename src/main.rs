2a99be9ed1a824429c78236cfb4b37e80601a37c
Ryder Lewis <ryder.lewis@gmail.com>
2022-12-03 11:24:50 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![allow(dead_code)]
 
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
+mod day2101;
+mod day2102;
+mod day2103;
+mod day2104;
+mod day2105;
+mod day2106;
+mod day2107;
+mod day2108;
+mod day2109;
 mod day2201;
 mod day2202;
 mod day2203;
@@ -37,15 +37,15 @@ fn main() {
     }
 
     match day {
-        1 => day1::run(part),
-        2 => day2::run(part),
-        3 => day3::run(part),
-        4 => day4::run(part),
-        5 => day5::run(part),
-        6 => day6::run(part),
-        7 => day7::run(part),
-        8 => day8::run(part),
-        9 => day9::run(part),
+        2101 => day2101::run(part),
+        2102 => day2102::run(part),
+        2103 => day2103::run(part),
+        2104 => day2104::run(part),
+        2105 => day2105::run(part),
+        2106 => day2106::run(part),
+        2107 => day2107::run(part),
+        2108 => day2108::run(part),
+        2109 => day2109::run(part),
         2201 => day2201::run(part),
         2202 => day2202::run(part),
         2203 => day2203::run(part),
@@ -54,6 +54,6 @@ fn main() {
 }
 
 fn print_usage(args: &Vec<String>) {
-    println!("Usage: {} [day] [part], where day is a number between 1 and 25, and part is 1 or 2", args[0]);
+    println!("Usage: {} [day] [part], where day is a number between 2101-2125 or 2201-2225, and part is 1 or 2", args[0]);
     exit(1);
 }
